e84a5522a96f2b9e6c2348987622a6ab99cd0867
Bryan Chan <bryan.chan@ualberta.ca>
2026-06-08 16:04:28 -0600

    update

---
+++ all_exps_2.sh
@@ -0,0 +1,22 @@
+### NO THINKING
+python thought_gridworld-rl.py --n_thought_acts=0 --seed=42 --output_file=debug_results/test_thought_env-no_thought_acts & 
+python thought_gridworld-rl.py --n_thought_acts=0 --seed=43 --output_file=debug_results/test_thought_env-no_thought_acts &
+python thought_gridworld-rl.py --n_thought_acts=0 --seed=44 --output_file=debug_results/test_thought_env-no_thought_acts &
+python thought_gridworld-rl.py --n_thought_acts=0 --seed=45 --output_file=debug_results/test_thought_env-no_thought_acts &
+python thought_gridworld-rl.py --n_thought_acts=0 --seed=46 --output_file=debug_results/test_thought_env-no_thought_acts &
+
+### THINKING x3
+python thought_gridworld-rl.py --n_thought_acts=3 --seed=42 --output_file=debug_results/test_thought_env-3_thought_acts & 
+python thought_gridworld-rl.py --n_thought_acts=3 --seed=43 --output_file=debug_results/test_thought_env-3_thought_acts &
+python thought_gridworld-rl.py --n_thought_acts=3 --seed=44 --output_file=debug_results/test_thought_env-3_thought_acts &
+python thought_gridworld-rl.py --n_thought_acts=3 --seed=45 --output_file=debug_results/test_thought_env-3_thought_acts &
+python thought_gridworld-rl.py --n_thought_acts=3 --seed=46 --output_file=debug_results/test_thought_env-3_thought_acts &
+
+### THINKING x10
+python thought_gridworld-rl.py --n_thought_acts=10 --seed=42 --output_file=debug_results/test_thought_env-10_thought_acts & 
+python thought_gridworld-rl.py --n_thought_acts=10 --seed=43 --output_file=debug_results/test_thought_env-10_thought_acts &
+python thought_gridworld-rl.py --n_thought_acts=10 --seed=44 --output_file=debug_results/test_thought_env-10_thought_acts &
+python thought_gridworld-rl.py --n_thought_acts=10 --seed=45 --output_file=debug_results/test_thought_env-10_thought_acts &
+python thought_gridworld-rl.py --n_thought_acts=10 --seed=46 --output_file=debug_results/test_thought_env-10_thought_acts &
+
+wait
