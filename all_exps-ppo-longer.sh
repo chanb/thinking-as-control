28eca4633d660428e89a4648da04c6a7b3710306
Bryan Chan <bryan.chan@ualberta.ca>
2026-07-24 05:16:30 -0600

    Update

--- all_exps-ppo-longer.sh
+++ all_exps-ppo-longer.sh
@@ -22,7 +22,7 @@ do
         n_thought_states=$1
         n_thought_acts=$2
 
-        python thought_gridworld-rl.py --domain=${domain} --desc=${desc} --ent_coef=0.02 --d_model=${d_model} --num_iterations=${num_iterations} --algo=ppo:reverse_kl --n_thought_states=${n_thought_states} --n_thought_acts=${n_thought_acts} --seed=${seed} --output_file=${results_dir}/n_thought_states_${n_thought_states}-n_thought_acts_${n_thought_acts} --log_file=${logs_dir}/n_thought_states_${n_thought_states}-n_thought_acts_${n_thought_acts} &
+        python thought_gridworld-rl.py --domain=${domain} --max_steps=1000 --desc=${desc} --ent_coef=0.02 --d_model=${d_model} --num_iterations=${num_iterations} --algo=ppo:reverse_kl --n_thought_states=${n_thought_states} --n_thought_acts=${n_thought_acts} --seed=${seed} --output_file=${results_dir}/n_thought_states_${n_thought_states}-n_thought_acts_${n_thought_acts} --log_file=${logs_dir}/n_thought_states_${n_thought_states}-n_thought_acts_${n_thought_acts} &
     done
 #     wait
 # done
@@ -31,7 +31,7 @@ do
 # for seed in 42
 # do
     # echo $seed
-    python thought_gridworld-rl.py --domain=${domain} --desc=${desc} --ent_coef=0.02 --d_model=${d_model} --num_iterations=${num_iterations} --algo=ppo:reverse_kl --n_thought_states=1 --n_thought_acts=0 --seed=${seed} --output_file=${results_dir}/no_thought_acts --log_file=${logs_dir}/no_thought_acts &
-    python thought_gridworld-rl.py --domain=${domain} --desc=${desc} --ent_coef=0.02 --d_model=${d_model} --num_iterations=${num_iterations} --algo=ppo:reverse_kl --n_thought_states=1 --n_thought_acts=0 --thought_per_state --seed=${seed} --output_file=${results_dir}/no_thought_acts-thought_per_state --log_file=${logs_dir}/no_thought_acts-thought_per_state &
+    python thought_gridworld-rl.py --domain=${domain} --max_steps=1000 --desc=${desc} --ent_coef=0.02 --d_model=${d_model} --num_iterations=${num_iterations} --algo=ppo:reverse_kl --n_thought_states=1 --n_thought_acts=0 --seed=${seed} --output_file=${results_dir}/no_thought_acts --log_file=${logs_dir}/no_thought_acts &
+    python thought_gridworld-rl.py --domain=${domain} --max_steps=1000 --desc=${desc} --ent_coef=0.02 --d_model=${d_model} --num_iterations=${num_iterations} --algo=ppo:reverse_kl --n_thought_states=1 --n_thought_acts=0 --thought_per_state --seed=${seed} --output_file=${results_dir}/no_thought_acts-thought_per_state --log_file=${logs_dir}/no_thought_acts-thought_per_state &
     wait
 done
